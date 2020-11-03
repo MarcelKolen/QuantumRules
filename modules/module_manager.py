1cccc36d6cc8d875c964021e55baada1f4d0f76c
Asqulum <71020683+Asqulum@users.noreply.github.com>
2020-11-03 19:28:54 +0100

    add superdense to modulemanager

--- modules/module_manager.py
+++ modules/module_manager.py
@@ -2,11 +2,13 @@ class ItemTypes:
     TEXT_ITEM = 'TI'
     MULTIPLE_CHOICE = 'MC'
     MULTIPLE_CHOICE_MULTIPLE_ANSWER = 'MA'
+    SUPER_DENSE = 'SD'
 
     ITEM_TYPE_CHOICES = [
         (TEXT_ITEM, 'Text Item'),
         (MULTIPLE_CHOICE, 'Multiple Choice'),
         (MULTIPLE_CHOICE_MULTIPLE_ANSWER, 'Multiple Choice multiple answer'),
+        (SUPER_DENSE, 'Superduper dense coding'),
     ]
 
 
@@ -44,6 +46,15 @@ def select_module(item_type, as_model=False, as_admin=False):
                 import modules.views.multipleChoiceMultipleAnswer.multiple_choice_multiple_answer_admin as module
             else:
                 import modules.views.multipleChoiceMultipleAnswer.multiple_choice_multiple_answer as module
+
+    elif item_type == ItemTypes.SUPER_DENSE:
+        if as_model is True:
+            from modules.models import superdense as module
+        else:
+            if as_admin is True:
+                import modules.views.superdense.super_dense_admin as module
+            else:
+                import modules.views.superdense.super_dense as module
 
     else:
         return False, None
